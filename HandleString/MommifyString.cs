4ed4428a70062a62d4b4862183d397936ed448e1
renlefei <renlefei16@163.com>
2019-04-11 14:15:54 +0800

    task_three:refactor02

--- HandleString/MommifyString.cs
+++ HandleString/MommifyString.cs
@@ -1,32 +1,21 @@
-﻿namespace HandleString
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace HandleString
 {
     public static class MommifyString
-    {       
+    {
+        public const string VowelString = "aeiou";
         public static string Mommify(string word)
         {
-            var count = 0;
-
-            for (var i = 0; i < word.Length; i++)
-            {
-                if("aeiou".IndexOf(word[i]) >=0)
-                    count+=1;
-            }
+            var count = CountVowels(word);
 
             var result = string.Empty;
 
             if ((count * 1.0) / word.Length >= 0.30)
             {
-                for (var i = 0; i < word.Length; i++)
-                {
-                    if ("aeiou".IndexOf(word[i]) >= 0)
-                    {
-                        result += "mommy";
-                    }
-                    else
-                    {
-                        result += word[i];
-                    }
-                }
+                result = Replace(word);
             }
             else
             {
@@ -34,6 +23,30 @@
             }
 
             return result;
+        }
+
+        private static string Replace(string word)
+        {
+            string result = String.Empty;
+            foreach (var t in word)
+            {
+                if (VowelString.IndexOf(t) >= 0)
+                {
+                    result += "mommy";
+                }
+                else
+                {
+                    result += t;
+                }
+            }
+
+            return result;
+        }
+
+        private static int CountVowels(string word)
+        {
+            var countVowels = word.Count(t => VowelString.IndexOf(t) >= 0);
+            return countVowels;
         }
     }
 }
